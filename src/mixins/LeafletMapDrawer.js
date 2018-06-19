c72f2b2cb32bc50404cea7a02620aeeb40c66753
Yernar <ernar@agrostream.net>
2018-06-19 17:25:46 +0600

    added leaflet markers

--- src/mixins/LeafletMapDrawer.js
+++ src/mixins/LeafletMapDrawer.js
@@ -5,8 +5,9 @@ import 'leaflet/dist/leaflet.css'
 export default {
   data() {
     return {
-      leafletMap: null,
-      myLatLng: {lat: 51.1605, lng: 71.4704},
+      leafMap: null,
+      leafLatLng: {lat: 51.1605, lng: 71.4704},
+      leafMarkers: new L.FeatureGroup(),
     }
   },
   mounted() {
@@ -14,17 +15,41 @@ export default {
   },
   methods: {
     drawMap() {
-      if (this.leafletMap) this.leafletMap.remove()
+      if (this.leafMap) this.leafMap.remove()
       this._addLayers()
+      this._addMarker()
+      //this._addTooltips()
+      this._addPopups()
     },
     _addLayers() {
-      let attribution = 'Kazdream Technologies';
-      let osm = L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {attribution: attribution});
-      let satellite = L.
-      tileLayer.provider('Esri.WorldImagery', {attribution: attribution});
-      let baseLayers = { "Спутник": satellite, "OpenStreetMaps": osm};
-      this.leafletMap = L.map('map', {editable: true, layers: [osm]}).setView(this.myLatLng, 15);
-      L.control.layers(baseLayers).addTo(this.leafletMap);
+      let attribution = 'Kazdream Technologies'
+      let osm = L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {attribution: attribution})
+      let satellite = L.tileLayer.provider('Esri.WorldImagery', {attribution: attribution})
+      let baseLayers = { "Спутник": satellite, "OpenStreetMaps": osm}
+      this.leafMap = L.map('map', {editable: true, layers: [osm]}).setView(this.leafLatLng, 15)
+      L.control.layers(baseLayers).addTo(this.leafMap)
     },
+    _addMarker() {
+      let customIcon = L.icon({iconUrl: require('@/assets/marker.png'), iconSize: [32, 32], iconAnchor: [15, 25], popupAnchor: [0, -16]})
+      let marker = L.marker([51.1605, 71.4704], {icon: customIcon}).addTo(this.leafMap)
+      marker.label = 'Kazakhstan<br/> Astana <br/> bridge!'
+      this.leafMarkers.addLayer(marker)
+      this.leafMap.addLayer(this.leafMarkers)
+    },
+    _addTooltips() {
+      this.leafMarkers.eachLayer(marker => {
+        marker.bindTooltip(marker.label, {
+          permanent: false,
+          direction: "center",
+          opacity: 1,
+          className: 'tooltip-transparent'
+        })
+      })
+    },
+    _addPopups() {
+      this.leafMarkers.eachLayer(marker => {
+        marker.bindPopup(marker.label)
+      })
+    }
   },
 }
